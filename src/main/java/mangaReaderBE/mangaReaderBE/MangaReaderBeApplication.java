3be2bc5ee2038ad98408abb1ed7460d74cc927a4
Asura0107 <mircoorosio@gmail.com>
2024-03-04 17:03:45 +0100

    Initial commit

---
+++ src/main/java/mangaReaderBE/mangaReaderBE/MangaReaderBeApplication.java
@@ -0,0 +1,13 @@
+package mangaReaderBE.mangaReaderBE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MangaReaderBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MangaReaderBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mangaReaderBE/mangaReaderBE/MangaReaderBeApplicationTests.java
@@ -0,0 +1,13 @@
+package mangaReaderBE.mangaReaderBE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MangaReaderBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
