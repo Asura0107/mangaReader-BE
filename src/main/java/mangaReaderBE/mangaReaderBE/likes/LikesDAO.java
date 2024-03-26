3defc73ecab2993754921bd46bbfff594862313d
Asura0107 <mircoorosio@gmail.com>
2024-03-26 21:33:09 +0100

    aggiunto like get

--- src/main/java/mangaReaderBE/mangaReaderBE/likes/LikesDAO.java
+++ src/main/java/mangaReaderBE/mangaReaderBE/likes/LikesDAO.java
@@ -1,8 +1,13 @@
 package mangaReaderBE.mangaReaderBE.likes;
 
+import mangaReaderBE.mangaReaderBE.Favorite.Favorite;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.UUID;
+
 @Repository
 public interface LikesDAO extends JpaRepository<Likes, Long> {
+    Likes findByUserIdAndId(UUID userId, Long id);
+
 }
